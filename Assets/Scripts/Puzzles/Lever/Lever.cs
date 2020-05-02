5c643b969ba3c8d62dbdd458d4c68c00752d701a
Giulio Solito <coolguy6318@hotmail.com>
2020-05-02 23:08:30 +0200

    Created Lever puzzle system, created Puzzle base script since most puzzles share same

---
+++ Assets/Scripts/Puzzles/Lever/Lever.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public bool _isFlippedOn = false;
+    private bool _canInteract = false;
+
+    public delegate void OnLeverChanged();
+    public static event OnLeverChanged onLeverChanged;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && _canInteract)
+        {
+            _isFlippedOn = !_isFlippedOn;
+
+            if (onLeverChanged != null)
+            {
+                onLeverChanged();
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _canInteract = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            _canInteract = false;
+        }
+    }
+}
